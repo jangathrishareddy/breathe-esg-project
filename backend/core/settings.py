fe7ca19c5441a971b8c7df1ab1e61285909d853a
jangathrishareddy <your-github-email>
2026-05-27 16:55:22 +0530

    added Procfile

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@^+$vp+w8-bc*ucvjax^lz)%t9aw9_0fs)6-30=1cmyrd&e*h&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [""]
 
 
 # Application definition
@@ -123,5 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 CORS_ALLOW_ALL_ORIGINS = True
